37012a3b0d40323c9c6812ffbd6211f2684b818f
Łukasz Sus <44533648+lukaszsus@users.noreply.github.com>
2021-06-18 10:09:33 +0200

    Tests refactored.

--- test/test_module_testing_wrapper.py
+++ test/test_module_testing_wrapper.py
@@ -1,12 +1,10 @@
 from dataclasses import dataclass
-from typing import Any, Tuple
-from unittest.mock import MagicMock
+from typing import Dict
 
 import pytest
 
 from magda.decorators import accept, produce, finalize
 from magda.module import Module
-from magda.module.results import ResultSet
 from magda.testing import ModuleTestingWrapper
 
 
@@ -26,19 +24,24 @@ class ProcessedData(Module.Interface):
 @accept(RawData)
 @produce(ProcessedData)
 @finalize
+class ModuleTakingRequestAndData(Module.Runtime):
+    def run(self, request, data: Module.ResultSet, **kwargs):
+        return ProcessedData(data.get(RawData)[::-1] + ' ' + request['text'])
+
+
+@accept(RawData)
+@produce(ProcessedData)
+@finalize
 class ModuleOnlyData(Module.Runtime):
-    """
-    Module has only 'data' argument in method run(). It is typical, expected use case.
-    """
-    @dataclass
-    class Parameters:
-        param: str
-
-    def bootstrap(self, **kwargs):
-        self.params: self.Parameters = self.Parameters(**self.parameters)
-
     def run(self, data: Module.ResultSet, **kwargs):
         return ProcessedData(data.get(RawData)[::-1])
+
+
+@produce(ProcessedData)
+@finalize
+class ModuleOnlyRequest(Module.Runtime):
+    def run(self, request: Dict[str, str], **kwargs):
+        return ProcessedData(request['text'])
 
 
 @accept(RawData, ProcessedData)
@@ -48,6 +51,14 @@ class ModuleManyDataElements(Module.Runtime):
     """
     Module takes 'data' argument which is ResultSet with many elements.
     """
+    def run(self, data: Module.ResultSet, **kwargs):
+        text_part_1 = data.get(RawData)[::-1]
+        text_part_2 = data.get(ProcessedData).data
+        return ProcessedData(text_part_1 + '_' + text_part_2)
+
+
+@finalize
+class ModuleTestForSettingParameters(Module.Runtime):
     @dataclass
     class Parameters:
         param: str
@@ -56,92 +67,64 @@ class ModuleManyDataElements(Module.Runtime):
         self.params: self.Parameters = self.Parameters(**self.parameters)
 
     def run(self, data: Module.ResultSet, **kwargs):
-        text_part_1 = data.get(RawData)[::-1]
-        text_part_2 = data.get(ProcessedData).data
-        return ProcessedData(text_part_1 + '_' + text_part_2)
+        return ProcessedData(data.get(RawData)[::-1])
 
 
-@accept(RawData)
-@produce(ProcessedData)
 @finalize
 class ModuleSync(Module.Runtime):
-    @dataclass
-    class Parameters:
-        param: str
-
     def bootstrap(self, **kwargs):
         self.is_built: bool = True
-        self.params: self.Parameters = self.Parameters(**self.parameters)
+        self.is_run: bool = False
 
     def run(self, request, data: Module.ResultSet, **kwargs):
-        self.request = request
-        self.data = data
-        return ProcessedData(data.get(RawData)[::-1])
+        self.is_run = True
+        return None
 
     def teardown(self):
         self.is_built: bool = False
         return super().teardown()
 
 
-@accept(RawData)
-@produce(ProcessedData)
 @finalize
 class ModuleAsync(Module.Runtime):
-    @dataclass
-    class Parameters:
-        param: str
-
     async def bootstrap(self, **kwargs):
         self.is_built: bool = True
-        self.params: self.Parameters = self.Parameters(**self.parameters)
+        self.is_run: bool = False
 
     async def run(self, request, data: Module.ResultSet, **kwargs):
-        return ProcessedData(data.get(RawData)[::-1])
+        self.is_run = True
+        return None
 
     async def teardown(self):
         self.is_built: bool = False
         return super().teardown()
 
 
-def run_mock_sync(self, request, data, **kwargs) -> Tuple[Any, Any]:
-    return request, data
-
-
-async def run_mock_async(self, request, data, **kwargs) -> Tuple[Any, Any]:
-    return request, data
-
-
-@pytest.mark.parametrize('module_cls', [ModuleAsync, ModuleSync])
 @pytest.mark.asyncio
 class TestModuleTestingWrapper:
     module_params = {'param': 'value'}
 
-    async def test_should_process_correctly(self, module_cls):
-        module = module_cls('my-module-a').set_parameters(TestModuleTestingWrapper.module_params)
-        mock = await ModuleTestingWrapper(module).build()
-
-        result = await mock.data(RawData('xyz')).request({'query': 'query'}).run()
-        assert result == ProcessedData('zyx')
-
-    async def test_should_process_correctly_many_data_elements(self, module_cls):
-        module = module_cls('my-module-a').set_parameters(TestModuleTestingWrapper.module_params)
-        mock = await ModuleTestingWrapper(module).build()
-
-        result = await mock.data(RawData('xyz')).request({'query': 'query'}).run()
-        assert result == ProcessedData('zyx')
-
+    @pytest.mark.parametrize('module_cls', [ModuleAsync, ModuleSync])
     async def test_should_correctly_bootstrap(self, module_cls):
-        module = module_cls('my-module-a').set_parameters(TestModuleTestingWrapper.module_params)
+        module = module_cls('test-module')
         mock = await ModuleTestingWrapper(module).build()
         assert mock.module.is_built
 
+    @pytest.mark.parametrize('module_cls', [ModuleAsync, ModuleSync])
     async def test_should_correctly_teardown(self, module_cls):
-        module = module_cls('my-module-a').set_parameters(TestModuleTestingWrapper.module_params)
+        module = module_cls('test-module')
         mock = await ModuleTestingWrapper(module).build()
         await mock.close()
         assert not mock.module.is_built
 
-    async def test_should_correctly_set_request(self, module_cls):
+    @pytest.mark.parametrize('module_cls', [ModuleAsync, ModuleSync])
+    async def test_should_correctly_invoke_run(self, module_cls):
+        module = module_cls('test-module')
+        mock = await ModuleTestingWrapper(module).build()
+        await mock.run()
+        assert mock.module.is_run
+
+    async def test_should_correctly_set_request(self):
         target_request = {'key': 'value'}
 
         @finalize
@@ -154,7 +137,7 @@ class TestModuleTestingWrapper:
         mock = mock.request(target_request)
         await mock.run()
 
-    async def test_should_correctly_set_data(self, module_cls):
+    async def test_should_correctly_set_data(self):
         raw_data = RawData('xyz')
 
         @finalize
@@ -167,44 +150,58 @@ class TestModuleTestingWrapper:
         mock = mock.data(raw_data)
         await mock.run()
 
-    async def test_should_not_compile(self, module_cls):
-        module = module_cls('my-module-a').set_parameters({})   # Missing required params
+    async def test_should_correctly_process_request_and_data(self):
+        module = ModuleTakingRequestAndData('test-module')
+        mock = await ModuleTestingWrapper(module).build()
 
-        with pytest.raises(TypeError):
-            await ModuleTestingWrapper(module).build()
+        result = await mock.request({'text': 'request_text'}).data(RawData('xyz')).run()
+        assert result == ProcessedData('zyx request_text')
 
-    async def test_should_correctly_pass_shared_parameters(self, module_cls):
-        shared_parameters = {'shared_param': 'value'}
-        module = module_cls('my-module-a').set_parameters(TestModuleTestingWrapper.module_params)
-        mock = await ModuleTestingWrapper(module).build(shared_parameters=shared_parameters)
-        assert mock.module.shared_parameters == shared_parameters
-
-    async def test_should_correctly_pass_context(self, module_cls):
-        def get_context():
-            return 'callable context'
-
-        module = module_cls('my-module-a').set_parameters(TestModuleTestingWrapper.module_params)
-        mock = await ModuleTestingWrapper(module).build(context=get_context)
-        assert mock.module.context() == get_context()
-
-
-class TestModuleTestingWrapperOnlyData:
-    @pytest.mark.asyncio
-    async def test_should_process_correctly(self):
-        module = ModuleOnlyData('my-module-a').set_parameters({'param': 'value'})
+    async def test_should_correctly_process_only_data(self):
+        module = ModuleOnlyData('test-module')
         mock = await ModuleTestingWrapper(module).build()
 
         mock = mock.data(RawData('xyz'))
         result = await mock.run()
         assert result == ProcessedData('zyx')
 
+    async def test_should_correctly_process_only_request(self):
+        module = ModuleOnlyRequest('test-module')
+        mock = await ModuleTestingWrapper(module).build()
 
-class TestModuleTestingWrapperManyDataElements:
-    @pytest.mark.asyncio
-    async def test_should_process_correctly(self):
-        module = ModuleManyDataElements('my-module-a').set_parameters({'param': 'value'})
+        mock = mock.request({'text': 'request_text'})
+        result = await mock.run()
+        assert result == ProcessedData('request_text')
+
+    async def test_should_correctly_process_many_data_elements(self):
+        module = ModuleManyDataElements('test-module')
         mock = await ModuleTestingWrapper(module).build()
 
         mock = mock.data(RawData('xyz'), ProcessedData('xyz'))
         result = await mock.run()
         assert result == ProcessedData('zyx_xyz')
+
+    async def test_should_build_when_parameters_are_passed(self):
+        module = ModuleTestForSettingParameters('test-module').set_parameters({'param': 'value'})
+        mock = await ModuleTestingWrapper(module).build()
+        assert isinstance(mock, ModuleTestingWrapper)
+
+    async def test_should_not_build_for_empty_parameters(self):
+        module = ModuleTestForSettingParameters('test-module').set_parameters({})
+
+        with pytest.raises(TypeError):
+            await ModuleTestingWrapper(module).build()
+
+    async def test_should_correctly_pass_shared_parameters(self):
+        shared_parameters = {'shared_param': 'value'}
+        module = ModuleSync('test-module')
+        mock = await ModuleTestingWrapper(module).build(shared_parameters=shared_parameters)
+        assert mock.module.shared_parameters == shared_parameters
+
+    async def test_should_correctly_pass_context(self):
+        def get_context():
+            return 'callable context'
+
+        module = ModuleSync('test-module')
+        mock = await ModuleTestingWrapper(module).build(context=get_context)
+        assert mock.module.context() == get_context()
