a223c08eb03d2a5d3068ac8c4822e60ea03d38dc
Jakub Błaszczyk <20283462+jamebs@users.noreply.github.com>
2021-06-30 06:27:39 +0000

    Add tests for pipeline logging

--- test/utils/logger/test_logger.py
+++ test/utils/logger/test_logger.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
 import re
-import logging
-from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
 from magda.utils.logger import MagdaLogger
@@ -20,7 +18,7 @@ class PartialOutput(str):
         return self in other and (re.search(self.COLOR_REGEXP, other) is not None) == self.color
 
 
-class TestMagdaLogger(TestCase):
+class TestMagdaLogger:
     @patch('magda.utils.logger.logger.print')
     def test_should_log_standard_message(self, mock):
         config = MagdaLogger.Config()
@@ -64,53 +62,56 @@ class TestMagdaLogger(TestCase):
         mock.assert_called_once_with(PartialOutput(message).with_color())
 
     @patch('magda.utils.logger.logger.print')
-    def test_logger_should_use_prints(self, print_mock):
-        with self.assertLogs(level=logging.INFO) as log:
-            config = MagdaLogger.Config(output=MagdaLogger.Config.Output.STDOUT)
-            logger = MagdaLogger.of(config)
-            message = 'Hello!'
-            logger.info(message)
+    @patch('magda.utils.logger.logger.getLogger')
+    def test_logger_should_use_prints(self, logging_mock, print_mock):
+        config = MagdaLogger.Config(output=MagdaLogger.Config.Output.STDOUT)
+        logger = MagdaLogger.of(config)
+        message = 'Hello!'
+        logger.info(message)
 
-            print_mock.assert_called_once_with(PartialOutput(message).with_color())
-            self.assertEqual(len(log.output), 0)
-
-    @patch('magda.utils.logger.logger.print')
-    def test_logger_should_use_native_logging(self, print_mock):
-        with self.assertLogs(level=logging.INFO) as log:
-            config = MagdaLogger.Config(output=MagdaLogger.Config.Output.LOGGING)
-            logger = MagdaLogger.of(config)
-            message = 'Hello!'
-            logger.info(message)
-
-            print_mock.assert_not_called()
-            self.assertEqual(len(log.output), 1)
-            self.assertIn(PartialOutput(message).with_color(), log.output[0])
+        print_mock.assert_called_once_with(PartialOutput(message).with_color())
+        logging_mock.assert_not_called()
 
     @patch('magda.utils.logger.logger.print')
-    def test_logger_should_use_custom_output(self, print_mock):
-        with self.assertLogs(level=logging.INFO) as log:
-            mock = MagicMock()
-            config = MagdaLogger.Config(output=mock)
-            logger = MagdaLogger.of(config)
-            message = 'Hello!'
-            logger.info(message)
+    @patch('magda.utils.logger.logger.getLogger')
+    def test_logger_should_use_native_logging(self, logging_mock, print_mock):
+        runtime_logger_mock = MagicMock()
+        logging_mock.return_value = runtime_logger_mock
 
-            print_mock.assert_not_called()
-            self.assertEqual(len(log.output), 0)
-            mock.assert_called_once_with(PartialOutput(message).with_color())
+        config = MagdaLogger.Config(output=MagdaLogger.Config.Output.LOGGING)
+        logger = MagdaLogger.of(config)
+        message = 'Hello!'
+        logger.info(message)
+
+        print_mock.assert_not_called()
+        logging_mock.assert_called_once()
+        runtime_logger_mock.info.assert_called_once_with(PartialOutput(message).with_color())
 
     @patch('magda.utils.logger.logger.print')
-    def test_logger_should_not_fail_with_non_callable_output(self, print_mock):
-        with self.assertLogs(level=logging.INFO) as log:
-            mock = 'Non-callable'
-            config = MagdaLogger.Config(output=mock)
-            logger = MagdaLogger.of(config)
-            message = 'Hello!'
+    @patch('magda.utils.logger.logger.getLogger')
+    def test_logger_should_use_custom_output(self, logging_mock, print_mock):
+        output_mock = MagicMock()
+        config = MagdaLogger.Config(output=output_mock)
+        logger = MagdaLogger.of(config)
+        message = 'Hello!'
+        logger.info(message)
 
-            logger.info(message)
+        print_mock.assert_not_called()
+        logging_mock.assert_not_called()
+        output_mock.assert_called_once_with(PartialOutput(message).with_color())
 
-            print_mock.assert_not_called()
-            self.assertEqual(len(log.output), 0)
+    @patch('magda.utils.logger.logger.print')
+    @patch('magda.utils.logger.logger.getLogger')
+    def test_logger_should_not_fail_with_non_callable_output(self, logging_mock, print_mock):
+        output_mock = 'Non-callable'
+        config = MagdaLogger.Config(output=output_mock)
+        logger = MagdaLogger.of(config)
+        message = 'Hello!'
+
+        logger.info(message)
+
+        print_mock.assert_not_called()
+        logging_mock.assert_not_called()
 
     def test_loggers_should_be_chained(self):
         request_name = 'RequestR1'
