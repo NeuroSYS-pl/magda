7b186ba78a8c72942e93537586146d3bf4881a8d
Jakub Błaszczyk <20283462+jamebs@users.noreply.github.com>
2021-09-01 07:00:02 +0000

    Update docusaurus

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -65,7 +65,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/NeuroSYS-pl/magda/edit/main/website/',
+          editUrl: 'https://github.com/NeuroSYS-pl/magda/edit/main/web/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
